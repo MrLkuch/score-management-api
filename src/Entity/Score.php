aad9de85566ed96c54707d3d7b8f0b2f5f235aeb
MrLkuch <kucheida.lucas@gmail.com>
2024-11-19 13:33:29 +0100

    build(): Initialisation API

---
+++ src/Entity/Score.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScoreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScoreRepository::class)]
+class Score
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'scores')]
+    private ?Team $team = null;
+
+    #[ORM\Column]
+    private ?int $points = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTeam(): ?Team
+    {
+        return $this->team;
+    }
+
+    public function setTeam(?Team $team): static
+    {
+        $this->team = $team;
+
+        return $this;
+    }
+
+    public function getPoints(): ?int
+    {
+        return $this->points;
+    }
+
+    public function setPoints(int $points): static
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+}
